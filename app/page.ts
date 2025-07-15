bb797d81ecc4cfcdffe45b0d405f581337138a06
07sumit1002 <sumitkumar101984@gmail.com>
2025-07-15 12:57:21 +0530

    `Removed HomePage component from app/page.tsx`

---
+++ app/page.ts
@@ -0,0 +1,7 @@
+// app/page.ts
+export const metadata = {
+  title: 'Teeksha Gupta - Portfolio',
+  description: 'Explore Teeksha Gupta’s creative projects, services, and more as a full-stack developer and designer.',
+};
+
+export { default } from './Homepage';
